f416b9593fc91b18ad14b1b8034e417751f977b6
Narjissi-fadwa <fadwa16narjissi@gmail.com>
2025-04-17 16:19:16 +0100

    method (PatientIn + Patientout )

--- public/js/app.js
+++ public/js/app.js
@@ -23,8 +23,12 @@ class Patients {
         this.healthState = healthState
         this.treatment = treatment
     }
-    GoTo() {
-
+    GoTo(prevlocation, destination) {
+        destination.people.push(this)
+        prevlocation.people.pop(this)
+        console.log(this.name + " khrej mn " + prevlocation.name);
+        console.log(this.name + " mcha l " + destination.name);
+        
     }
     pay(treatment) {
 
@@ -46,10 +50,12 @@ let Semicolon = new Patients("Semicolon", "SyntaxError", 60, "Empty", "Ill", [])
 // | Debugger  | 0     | [Cat]  |            |            |             |
 
 class Doctor {
-    constructor(name, money, office) {
+    constructor(name, money, office, waitingroom , consultationroom) {
         this.name = name
         this.money = money
         this.office = office
+        this.waitingroom = waitingroom
+        this.consultationroom = consultationroom
     }
     diagnosis(patient) {
         for (let i = 0; i < diagnosisGrid.length; i++) {
@@ -60,14 +66,37 @@ class Doctor {
             }
         }
     }
-    PatientIn() {
-
+    PatientIn(patient) {
+        if (this.consultationroom.people.length == 0) {
+            this.consultationroom.people.push(patient);
+            console.log(patient.name + " in the " + this.consultationroom.name);
+            // this.waitingroom.people.splice(patient , 1);
+        }else{
+            this.waitingroom.people.push(patient);
+            console.log(patient.name + " in the " + this.waitingroom.name);
+        }
+        
     }
-    Patientout() {
+    Patientout(patient) {
+        this.waitingroom.people.push(patient);
+        this.consultationroom.people.splice(patient , 1);
+        console.log(patient.name + " out of " + this.consultationroom.name);
 
     }
 }
-const doctor = new Doctor("Debugger", 0, []);
+let waitingroom = {
+    name: "waitingroom",
+    people: []
+}
+let office = {
+    name: "doctor Debugger's office",
+    people: []
+}
+let consultationroom = {
+    name : "consultationroom",
+    people: [],
+}
+const doctor = new Doctor("Debugger", 0, office , waitingroom , consultationroom);
 
 // ### Diagnosis Grid:
 
@@ -115,8 +144,20 @@ let Pharmacy = {
     box: [],
 
 }
-doctor.diagnosis(Optimus)
+let home = {
+    name: "home",
+    people: [Marcus, Optimus, Sangoku, DarthVader, Semicolon]
+}
 
+Marcus.GoTo(home , doctor.office)
+doctor.PatientIn(Marcus)
+
+Optimus.GoTo(home , doctor.office)
+doctor.PatientIn(Optimus)
+
+
+// doctor.diagnosis(Optimus)
+// doctor.waitingroom.people.push(Marcus)
 // Patients will then go to the pharmacy and receive their treatment if they have enough money. If they have enough money, they will be in good health; otherwise, they will be dead, and you will need to push them into a cemetery.
 
 // ### Treatment Rates:
